a919931011421c669054580a4d9d9ac97fc3a4a1
rchan26 <rchan@turing.ac.uk>
2021-11-24 13:29:51 +0000

    starting to implement bayesian fusion

---
+++ R/temporal_mesh_proposal_simulation.R
@@ -0,0 +1,65 @@
+construct_V_vanilla <- function(s, t, end_time, C, d) {
+  C1 <- ((t-s)*(end_time-t)) / (end_time-s)
+  C2 <- ((t-s)^2)/(C*(end_time-s))
+  Sigma <- matrix(data = NA,  nrow = C, ncol = C)
+  for (i in 1:C) {
+    for (j in 1:C) {
+      Sigma[i,j] <- C2
+      if (i==j) {
+        Sigma[i,j] <- Sigma[i,j] + C1
+      }
+    }
+  }
+  V <- kronecker(Sigma, diag(1, d))
+  return(list('V' = solve(V_inv), 'V_inv' = V_inv))
+}
+
+construct_M_vanilla <- function(s, t, ent_time, C, d, sub_posterior_samples) {
+  sum_vec <- sub_posterior_samples[[C]])
+  for (i in 1:(C-1)) {
+    sum_vec <- sum_vec + sub_posterior_samples[[i]]
+  }
+  x_bar <- sum_vec / C
+  C1 <- (end_time-t)/(end_time-s)
+  C2 <- (t-s)/(end_time-s)
+  M <- rep(NA, C*d)
+  for (i in 1:C) {
+    i_fill <- d*(i-1)+(1:d)
+    M[i_fill] <- C1*sub_posterior_samples[[i]] + C2*x_bar
+  }
+  return(M)
+}
+
+construct_V <- function(s, t, end_time, C, d, Lambda, inv_precondition_matrices) {
+  C1 <- (end_time-s) / ((t-s)*(end_time-t))
+  C2 <- -1/(end_time-t)
+  V_inv <- matrix(data = NA, nrow = C*d, ncol = C*d)
+  for (i in 1:C) {
+    for (j in 1:C) {
+      i_fill <- d*(i-1)+(1:d)
+      j_fill <- d*(j-1)+(1:d)
+      V_inv[i_fill,j_fill] <- C2*(inv_precondition_matrices[[i]]%*%Lambda%*%inv_precondition_matrices[[j]])
+      if (i==j) {
+        V_inv[i_fill,j_fill] <- V_inv[i_fill,j_fill] + C1*inv_precondition_matrices[[i]]
+      }
+    }
+  }
+  return(list('V' = solve(V_inv), 'V_inv' = V_inv))
+}
+
+construct_L_inv <- function(inv_precondition_matrices) {
+  L_inv <- matrix(data = NA, nrow = C*d, ncol = C*d)
+  for (i in 1:C) {
+    i_fill <- d*(i-1)+(1:d)
+    L_inv[i_fill,i_fill] <- inv_precondition_matrices[[i]]
+  }
+  return(L_inv)
+}
+
+construct_M <- function(s, t, end_time, C, d, Lambda, inv_precondition_matrices, sub_posterior_samples) {
+  covariance_mat <- construct_V(s, t, end_time, C, d, Lambda, inv_precondition_matrices)
+  V <- covariance_mat$V
+  L_inv <- construct_L_inv(inv_precondition_matrices)
+  x <- unlist(sub_posterior_samples)
+  return(V%*%L_inv%*%x / (t-s))
+}
