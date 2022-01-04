1652fffc3c1a5d501ad20b3ad5d0ee43aaedeefc
Ryan Chan <ryanchan2607@gmail.com>
2022-01-04 09:58:06 +0000

    simulated data generalised bayesian fusion scripts (still need to implement the guidance to choose the hyperparameters for the algorithm)

--- scripts/logistic_regression/simulated_data/SD4.R
+++ scripts/logistic_regression/simulated_data/SD4.R
@@ -1,63 +1,6 @@
 library(DCFusion)
 library(HMCBLR)
 
-# function to simulate data given frequencies of the coefficients and the coefficients themselves
-simulate_data <- function(N,
-                          alpha,
-                          frequencies,
-                          coefficients,
-                          G_means = rep(1, length(frequencies)),
-                          G_sds = rep(1, length(frequencies)),
-                          seed = NULL) {
-  if (length(frequencies) != length(coefficients)) {
-    stop("length of frequencies and length of coefficients are not the same")
-  }
-  if (!is.null(seed)) {
-    set.seed(seed)
-  }
-  dim <- length(frequencies)
-  X <- matrix(nrow = N, ncol = dim)
-  for (d in 1:dim) {
-    for (i in 1:N) {
-      if (runif(1, 0, 1) < frequencies[d]) {
-        X[i,d] <- rnorm(1, G_means[d], G_sds[d])
-      } else {
-        X[i,d] <- 0
-      }
-    }
-  }
-  y <- rep(0, N)
-  for (i in 1:N) {
-    beta_X <- alpha + sum(X[i,] * coefficients)
-    prob <- 1 / (1+exp(-beta_X))
-    y[i] <- rbinom(1, 1, prob = prob)
-  }
-  return(cbind(y, data.frame(X)))
-}
-
-# function to check the frequencies of 'active' variables in the dataset
-check_activity <- function(data, proportion = T) {
-  if (!is.data.frame(data)) {
-    stop("check_activity: data is not in a data frame format")
-  }
-  # create new data frame with same column names
-  active_df <- data.frame(matrix(nrow = 1, ncol = ncol(data)))
-  colnames(active_df) <- colnames(data)
-  # loop through columns and check how many are 'active'
-  # i.e. have a 1 in the instance
-  for (j in 1:ncol(data)) {
-    if (proportion) {
-      active_df[,j] <- sum(data[,j]!=0) / nrow(data)
-    } else {
-      active_df[,j] <- sum(data[,j]!=0)
-    }
-  }
-  print('proportion that each variable is active in the dataset:')
-  for (j in 1:ncol(active_df)) {
-    print(paste(colnames(active_df)[j], ':', active_df[1,j]))
-  }
-}
-
 ##### Initialise example #####
 seed <- 2021
 set.seed(seed)
@@ -69,11 +12,11 @@ true_beta <- c(-3, 1.2, -0.5, 0.8, 3)
 frequencies <- c(0.2, 0.3, 0.5, 0.01) # must have length = length(true_beta)-1
 
 # simulate data set
-simulated_data <- simulate_data(N = ndata,
-                                alpha = true_beta[1],
-                                frequencies = frequencies,
-                                coefficients = true_beta[2:length(true_beta)],
-                                seed = seed) 
+simulated_data <- simulate_LR_data(N = ndata,
+                                   alpha = true_beta[1],
+                                   frequencies = frequencies,
+                                   coefficients = true_beta[2:length(true_beta)],
+                                   seed = seed) 
 
 # check activity of the parameters
 check_activity(simulated_data)
