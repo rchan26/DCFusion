0a677a4313a9824e21b1b21f2f2bfc71204ec952
Ryan Chan <ryanchan2607@gmail.com>
2022-01-25 18:42:27 +0000

    starting implementation of guidance;

--- R/vanilla_BF_guidance.R
+++ R/vanilla_BF_guidance.R
@@ -1,23 +1,30 @@
-# # guidance for T
-# # uses guidance to choose T \geq C^{3/2}/m and then we look at multiples of that
-# # pass in a required ESS for initialise the algorithm
-# # We keep choosing multiples of that until we reach our required ESS
-# 
-# # ---------- first importance sampling step
-# particles <- rho_IS_multivariate(particles_to_fuse = particles_to_fuse,
-#                                  dim = dim,
-#                                  N = N,
-#                                  m = m,
-#                                  time = time_mesh[length(time_mesh)],
-#                                  inv_precondition_matrices = rep(list(diag(1, dim)), m),
-#                                  inverse_sum_inv_precondition_matrices = inverse_sum_matrices(rep(list(diag(1, dim)), m)),
-#                                  number_of_steps = length(time_mesh),
-#                                  time_mesh = time_mesh,
-#                                  resampling_method = resampling_method,
-#                                  seed = seed,
-#                                  n_cores = n_cores,
-#                                  cl = cl)
-# particle_set$CESS[1]
-# 
-# 
-# # guidance for P and n
+SH_T <- function() {
+  # compute T in SH setting
+}
+
+SSH_T <- function() {
+  # compute T in SSH setting
+}
+
+adaptive_mesh <- function() {
+  # use full mesh guidance for step j
+}
+
+regular_mesh <- function() {
+  # use regular mesh (don't compute average variation of trajectories)
+}
+
+# since the regular mesh can be computed prior to calling the algorithm,
+# but adaptive mesh must be computed at each stage of the algorithm,
+# then I need to have some sort of flag or logical variable which calls the adaptive
+# mesh at each iteration of the algorithm. If adaptive==FALSE, we continue using
+# the time mesh that is passed, otherwise, we compute one from the guidance.
+# Maybe we can just set time_mesh = 'adaptive' to do this...
+
+vanilla_SH <- function() {
+  
+}
+
+vanilla_SSH <- function() {
+  
+}
