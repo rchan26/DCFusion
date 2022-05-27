93c0e625f1fe5558b60ef8d918fc56198e93e794
Ryan Chan <ryanchan2607@gmail.com>
2022-05-27 10:55:40 +0100

    attempt at doing robust regression examples

--- scripts/robust_regression/house_prices/HP4.R
+++ scripts/robust_regression/house_prices/HP4.R
@@ -10,8 +10,8 @@ warmup <- 10000
 time_choice <- 1
 nu <- 5
 sigma <- 1
-prior_means <- rep(0, 11)
-prior_variances <- rep(10, 11)
+prior_means <- rep(0, 12)
+prior_variances <- rep(10, 12)
 ESS_threshold <- 0.5
 CESS_0_threshold <- 0.5
 CESS_j_threshold <- 0.2
@@ -20,66 +20,84 @@ n_cores <- parallel::detectCores()
 
 ##### Loading in Data #####
 
-load_bs_data <- function(file, seed = NULL) {
+load_house_prices_data <- function(file, seed = NULL) {
   original_data <- read.csv(file)
-  bike_sharing <- data.frame(rented = original_data$cnt,
-                             spring = as.numeric(original_data$season == 2),
-                             summer = as.numeric(original_data$season == 3),
-                             fall = as.numeric(original_data$season == 4),
-                             weekend = as.numeric(original_data$weekday %in% c(6, 0)),
-                             holiday = original_data$holiday,
-                             rush_hour = as.numeric(!(original_data$weekday %in% c(6, 0)) 
-                                                    & (original_data$hr %in% c(7, 8, 9, 16, 17, 18, 19))),
-                             weather = as.numeric(original_data$weathersit == 1),
-                             atemp = original_data$atemp,
-                             humid = original_data$hum,
-                             wind = original_data$windspeed)
-  bike_sharing <- bike_sharing[complete.cases(bike_sharing),]
+  house_prices <- data.frame(price = original_data$price/1000,
+                             bedrooms = original_data$bedrooms,
+                             bathrooms = original_data$bathrooms,
+                             sqft_living = original_data$sqft_living,
+                             sqft_lot = original_data$sqft_lot,
+                             sqft_above = original_data$sqft_above,
+                             sqft_basement = original_data$sqft_basement,
+                             floors = original_data$floors,
+                             condition = as.numeric(original_data$condition > 3),
+                             view = as.numeric(original_data$view > 0),
+                             avg_grade = as.numeric(7 <= original_data$grade & original_data$grade <= 9),
+                             good_grade = as.numeric(original_data$grade > 9))
+  house_prices <- house_prices[complete.cases(house_prices),]
   if (!is.null(seed)) {
     set.seed(seed)
-    bike_sharing <- bike_sharing[sample(1:nrow(bike_sharing)),]
+    house_prices <- house_prices[sample(1:nrow(house_prices)),]
   }
-  X <- subset(bike_sharing, select = -c(rented))
+  means <- list('sqft_living' = mean(house_prices$sqft_living),
+                'sqft_lot' = mean(house_prices$sqft_lot),
+                'sqft_above' = mean(house_prices$sqft_above),
+                'sqft_basement' = mean(house_prices$sqft_basement))
+  sds <- list('sqft_living' = sd(house_prices$sqft_living),
+              'sqft_lot' = sd(house_prices$sqft_lot),
+              'sqft_above' = sd(house_prices$sqft_above),
+              'sqft_basement' = sd(house_prices$sqft_basement))
+  house_prices$sqft_living <- (house_prices$sqft_living-means$sqft_living) / sds$sqft_living
+  house_prices$sqft_lot <- (house_prices$sqft_lot-means$sqft_lot) / sds$sqft_lot
+  house_prices$sqft_above <- (house_prices$sqft_above-means$sqft_above) / sds$sqft_above
+  house_prices$sqft_basement <- (house_prices$sqft_basement-means$sqft_basement) / sds$sqft_basement
+  X <- subset(house_prices, select = -c(price))
   design_mat <- as.matrix(cbind(rep(1, nrow(X)), X))
   colnames(design_mat)[1] <- 'intercept'
   return(list('data' = cbind(subset(design_mat, select = -c(intercept)),
-                             'rented' = bike_sharing$rented),
-              'y' = bike_sharing$rented,
-              'X' = design_mat))
+                             'price' = house_prices$price),
+              'y' = house_prices$price,
+              'X' = design_mat,
+              'variable_means' = means,
+              'variable_sds' = sds))
 }
 
-bike_sharing <- load_bs_data('scripts/robust_regression/bike_sharing/hour.csv', seed = seed)
+house_prices <- load_house_prices_data('scripts/robust_regression/house_prices/kc_house_data.csv', seed = seed)
 
 ##### Sampling from full posterior #####
-full_posterior <- hmc_sample_BRR(noise_error = 'student_t',
-                                 y = bike_sharing$y,
-                                 X = bike_sharing$X,
-                                 C = 1,
-                                 nu = nu,
-                                 sigma = sigma,
-                                 prior_means = prior_means,
-                                 prior_variances = prior_variances,
-                                 iterations = nsamples_MCF + 10000,
-                                 warmup = 10000,
-                                 chains = 1,
-                                 seed = seed,
-                                 output = T)
+
+full_posterior <-  hmc_sample_BRR(noise_error = 'student_t',
+                                  y = house_prices$y,
+                                  X = house_prices$X,
+                                  C = 1,
+                                  nu = nu,
+                                  sigma = sigma,
+                                  prior_means = prior_means,
+                                  prior_variances = prior_variances,
+                                  iterations = nsamples_MCF + warmup,
+                                  warmup = warmup,
+                                  chains = 1,
+                                  seed = seed,
+                                  output = T)
+
+apply(full_posterior, 2, mean)
 
 ##### Sampling from sub-posterior C=4 #####
 
-data_split_4 <- split_data(bike_sharing$data,
-                           y_col_index = 11,
-                           X_col_index = 1:10,
+data_split_4 <- split_data(house_prices$data,
+                           y_col_index = 12,
+                           X_col_index = 1:11,
                            C = 4,
                            as_dataframe = F)
-sub_posteriors_4 <- hmc_base_sampler_BRR(nsamples = nsamples_MCF,
+sub_posteriors_4 <- hmc_base_sampler_BRR(noise_error = 'student_t',
+                                         nsamples = nsamples_MCF,
                                          data_split = data_split_4,
                                          C = 4,
                                          nu = nu,
                                          sigma = sigma,
                                          prior_means = prior_means,
                                          prior_variances = prior_variances,
-                                         warmup = 10000,
+                                         warmup = warmup,
                                          seed = seed,
                                          output = T)
 
@@ -99,34 +117,9 @@ weierstrass_importance_4 <- weierstrass(Samples = sub_posteriors_4,
 weierstrass_rejection_4 <- weierstrass(Samples = sub_posteriors_4,
                                        method = 'reject')
 
-# ##### Poisson (Hypercube Centre) #####
-# print('Poisson Fusion (hypercube centre)')
-# Poisson_hc_4 <- bal_binary_fusion_SMC_BRR(N_schedule = rep(nsamples_MCF, 2),
-#                                           m_schedule = rep(2, 2),
-#                                           time_schedule = rep(time_choice, 2),
-#                                           base_samples = sub_posteriors_4,
-#                                           L = 3,
-#                                           dim = 8,
-#                                           data_split = data_split_4,
-#                                           nu = nu,
-#                                           sigma = sigma,
-#                                           prior_means = prior_means,
-#                                           prior_variances = prior_variances,
-#                                           C = 4,
-#                                           precondition = TRUE,
-#                                           resampling_method = 'resid',
-#                                           ESS_threshold = ESS_threshold,
-#                                           cv_location = 'hypercube_centre',
-#                                           diffusion_estimator = 'Poisson',
-#                                           seed = seed,
-#                                           n_cores = n_cores)
-# Poisson_hc_4$particles <- resample_particle_y_samples(particle_set = Poisson_hc_4$particles[[1]],
-#                                                       multivariate = TRUE,
-#                                                       resampling_method = 'resid',
-#                                                       seed = seed)
-# Poisson_hc_4$proposed_samples <- Poisson_hc_4$proposed_samples[[1]]
-# print(integrated_abs_distance(full_posterior, Poisson_hc_4$particles$y_samples))
-
+for (i in 1:4) {
+  print(apply(sub_posteriors_4[[i]], 2, mean))
+}
 ##### NB (Hypercube Centre) #####
 print('NB Fusion (hypercube centre)')
 NB_hc_4 <- bal_binary_fusion_SMC_BRR(N_schedule = rep(nsamples_MCF, 2),
@@ -134,7 +127,7 @@ NB_hc_4 <- bal_binary_fusion_SMC_BRR(N_schedule = rep(nsamples_MCF, 2),
                                      time_schedule = rep(time_choice, 2),
                                      base_samples = sub_posteriors_4,
                                      L = 3,
-                                     dim = 8,
+                                     dim = 5,
                                      data_split = data_split_4,
                                      nu = nu,
                                      sigma = sigma,
@@ -163,7 +156,7 @@ GBF_4 <- list('reg' = bal_binary_GBF_BRR(N_schedule = nsamples_GBF,
                                          time_mesh = NULL,
                                          base_samples = sub_posteriors_4,
                                          L = 2,
-                                         dim = 8,
+                                         dim = 5,
                                          data_split = data_split_4,
                                          nu = nu,
                                          sigma = sigma,
@@ -185,7 +178,7 @@ GBF_4 <- list('reg' = bal_binary_GBF_BRR(N_schedule = nsamples_GBF,
                                               time_mesh = NULL,
                                               base_samples = sub_posteriors_4,
                                               L = 2,
-                                              dim = 8,
+                                              dim = 5,
                                               data_split = data_split_4,
                                               nu = nu,
                                               sigma = sigma,
@@ -208,7 +201,7 @@ GBF_4$reg$particles <- resample_particle_y_samples(particle_set = GBF_4$reg$part
                                                    multivariate = TRUE,
                                                    resampling_method = 'resid',
                                                    seed = seed)
-print(integrated_abs_distance(full_posterior, GBF_4$reg$particles$y_samples)) 
+print(integrated_abs_distance(full_posterior, GBF_4$reg$particles$y_samples))
 compare_samples_bivariate(posteriors = list(full_posterior,
                                             GBF_4$reg$proposed_samples[[1]],
                                             GBF_4$reg$particles$y_samples),
@@ -227,12 +220,12 @@ compare_samples_bivariate(posteriors = list(full_posterior,
                           common_limit = c(-4, 4))
 
 ##### bal binary combining two sub-posteriors at a time #####
-balanced_C4 <- list('reg' = bal_binary_GBF_BRR(N_schedule = rep(nsamples_GBF, 2),
+balanced_C4 <- list('reg' = bal_binary_GBF_BRR(N_schedule = rep(nsamples_DCGBF, 2),
                                                m_schedule = rep(2, 2),
                                                time_mesh = NULL,
                                                base_samples = sub_posteriors_4,
                                                L = 3,
-                                               dim = 8,
+                                               dim = 5,
                                                data_split = data_split_4,
                                                nu = nu,
                                                sigma = sigma,
@@ -249,12 +242,12 @@ balanced_C4 <- list('reg' = bal_binary_GBF_BRR(N_schedule = rep(nsamples_GBF, 2)
                                                                       'vanilla' = FALSE),
                                                diffusion_estimator = diffusion_estimator,
                                                seed = seed),
-                    'adaptive' = bal_binary_GBF_BRR(N_schedule = rep(nsamples_GBF, 2),
+                    'adaptive' = bal_binary_GBF_BRR(N_schedule = rep(nsamples_DCGBF, 2),
                                                     m_schedule = rep(2, 2),
                                                     time_mesh = NULL,
                                                     base_samples = sub_posteriors_4,
                                                     L = 3,
-                                                    dim = 8,
+                                                    dim = 5,
                                                     data_split = data_split_4,
                                                     nu = nu,
                                                     sigma = sigma,
@@ -277,23 +270,15 @@ balanced_C4$reg$particles <- resample_particle_y_samples(particle_set = balanced
                                                          multivariate = TRUE,
                                                          resampling_method = 'resid',
                                                          seed = seed)
+balanced_C4$reg$proposed_samples <- balanced_C4$reg$proposed_samples[[1]]
 print(integrated_abs_distance(full_posterior, balanced_C4$reg$particles$y_samples))
-compare_samples_bivariate(posteriors = list(full_posterior,
-                                            balanced_C4$reg$proposed_samples[[1]],
-                                            balanced_C4$reg$particles$y_samples),
-                          colours = c('black', 'green', 'red'),
-                          common_limit = c(-4, 4))
 # adaptive mesh
 balanced_C4$adaptive$particles <- resample_particle_y_samples(particle_set = balanced_C4$adaptive$particles[[1]],
                                                               multivariate = TRUE,
                                                               resampling_method = 'resid',
                                                               seed = seed)
+balanced_C4$adaptive$proposed_samples <- balanced_C4$adaptive$proposed_samples[[1]]
 print(integrated_abs_distance(full_posterior, balanced_C4$adaptive$particles$y_samples))
-compare_samples_bivariate(posteriors = list(full_posterior,
-                                            balanced_C4$adaptive$proposed_samples[[1]],
-                                            balanced_C4$adaptive$particles$y_samples),
-                          colours = c('black', 'green', 'red'),
-                          common_limit = c(-4, 4))
 
 ##### IAD #####
 
@@ -310,4 +295,4 @@ integrated_abs_distance(full_posterior, neiswanger_false_4$samples)
 integrated_abs_distance(full_posterior, weierstrass_importance_4$samples)
 integrated_abs_distance(full_posterior, weierstrass_rejection_4$samples)
 
-save.image('BS4.RData')
+save.image('HP4.RData')
